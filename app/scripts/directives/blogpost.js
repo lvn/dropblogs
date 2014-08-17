263071404dd67976a2ade7ba1234b51528494634
Elvin Yung <elvinyung1@gmail.com>
2014-08-17 15:14:06 -0400

    removed navbar

--- app/scripts/directives/blogpost.js
+++ app/scripts/directives/blogpost.js
@@ -9,7 +9,10 @@
 angular.module('dropblogsApp')
   .directive('blogPost', function () {
     return {
-      template: '<div></div>',
+      template: '<h3><a href="#/post/{{post.path}}">{{post.title}}</a></h3>
+      {{post.path}}
+      <p>{{post.uploadDate}}</p>
+      <code>{{post.content}}</code>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         element.text('this is the blogPost directive');
